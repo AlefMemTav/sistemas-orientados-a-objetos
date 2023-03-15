05301810cfc714d6cb5c7fefc69fed7964cd1b35
AlefMemTav <190115548@aluno.unb.br>
2023-03-15 12:11:50 -0300

    Atualizando exercícios

--- src/capitulo6/exercicios/exercicio2/Principal.java
+++ src/capitulo6/exercicios/exercicio2/Principal.java
@@ -3,20 +3,26 @@ package capitulo6.exercicios.exercicio2;
 import java.util.Scanner;
 
 public class Principal {
+
     public static void main(String[] args) {
-        Scanner teclado = new Scanner (System.in);
+        Scanner teclado = new Scanner(System.in);
         System.out.println("Digite uma palavra: ");
         String palavra = teclado.nextLine();
-        String invertida="";
-        for (int i=palavra.length()-1; i >=0; i--){
-            invertida +=palavra.charAt(i);
+
+        String palavraLowerCase = palavra.toLowerCase();
+
+        String invertida = "";
+        for (int i = palavra.length() - 1; i >= 0; i--) {
+            invertida += palavra.charAt(i);
         }
+
+        String invertidaLowerCase = invertida.toLowerCase();
+
         System.out.println("Palavra invertida: " + invertida);
-        if (invertida.equals(palavra)){
+        if (invertidaLowerCase.equals(palavraLowerCase)) {
             System.out.println("Palindromo!");
+        } else {
+            System.out.println("Não é palindromo!");
         }
-        else{
-            System.out.println("Não é Palindromo!");
-        }
-   }
+    }
 }
--- src/capitulo6/exercicios/exercicio3/Principal.java
+++ src/capitulo6/exercicios/exercicio3/Principal.java
@@ -1,12 +1,18 @@
 package capitulo6.exercicios.exercicio3;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Principal {
+
     public static void main(String[] args) {
-        Scanner teclado = new Scanner (System.in);
-        System.out.println("Digite um número de ponto flutuante (float): ");
-        float numero = teclado.nextFloat();
-        String palavra = String.valueOf(numero);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número de ponto flutuante (Ex: 1,2): ");
+        float numero = scanner.nextFloat();
+
+        DecimalFormat formato = new DecimalFormat("#.##");
+        String palavra = formato.format(numero);
+
+        System.out.println("O número formatado é: " + palavra);
     }
 }
